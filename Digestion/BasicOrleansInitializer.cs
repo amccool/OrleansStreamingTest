484467c246b93656869c1a7b43f9b7b7ea511ab2
Alex McCool <alex.mccool@yahoo.com>
2016-01-28 23:39:50 -0800

    just becuase

---
+++ Digestion/BasicOrleansInitializer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digestion
+{
+    public sealed class BasicOrleansInitializer
+    {
+        private readonly static TraceSource _traceSource = new TraceSource("ProvingGroundActorDirectMessaging.BasicOrleansInitializer", SourceLevels.Error);
+
+        public static event EventHandler<EventArgs> OrleansInitComplete //pass-through to OrleansClientGlobalWrapper
+        {
+            add { OrleansClientGlobalWrapper.OrleansInitComplete += value; }
+            remove { OrleansClientGlobalWrapper.OrleansInitComplete -= value; }
+        }
+
+        public static bool IsInitialized
+        {
+            get
+            {
+                return OrleansClientGlobalWrapper.IsInitialized;
+            }
+        }
+
+        #region Singleton
+        private BasicOrleansInitializer() { }
+
+        private static readonly Lazy<BasicOrleansInitializer> lazy = new Lazy<BasicOrleansInitializer>(() => new BasicOrleansInitializer());
+        private static BasicOrleansInitializer Instance { get { return lazy.Value; } }
+        #endregion Singleton
+
+        public static void BeginInit()
+        {
+            OrleansClientGlobalWrapper.EnsureInitialized();
+            _traceSource.TraceEvent(TraceEventType.Verbose, 57, "Orleans Init has started");
+        }
+    }
+
+}
---
+++ Digestion/OrleansInitializer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digestion
+{
+    public static class OrleansInitializer
+    {
+        private readonly static TraceSource _traceSource = new TraceSource("R5Ent.ProvingGround.ProvingGroundCommFactory", SourceLevels.Error);
+
+        public static event EventHandler<EventArgs> OrleansInitComplete;
+
+        public static bool IsInitialized
+        {
+            get
+            {
+                return BasicOrleansInitializer.IsInitialized;
+            }
+        }
+
+        public static void BeginInit()
+        {
+#if USE_SERVICE_BUS
+#else
+            BasicOrleansInitializer.OrleansInitComplete += OrleansInitializer_OrleansInitComplete;
+            BasicOrleansInitializer.BeginInit();
+            _traceSource.TraceEvent(TraceEventType.Verbose, 57, "OrleansInitializer.BeginInit() started");
+#endif //USE_SERVICE_BUS
+        }
+
+        private static void OrleansInitializer_OrleansInitComplete(object sender, EventArgs e)
+        {
+            OrleansInitComplete(sender, e);
+        }
+    }
+}
